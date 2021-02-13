b05fdf9de964c0983357fb0bb7fbc66c2e0711f1
Griffin Barlow <griffinbrlw@gmail.com>
2021-02-12 22:04:38 -0500

    Added image numbers

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,11 +1,22 @@
+
 const list = document.querySelector('ul')
+const images = document.querySelectorAll('li')
 
 const totalImages = 7
 const visibleImages = 3
 const maxPage = Math.ceil(totalImages/visibleImages)
 
+let i = 1
 let position = 0
 let currentPage = 1
+
+
+    images.forEach(li => {
+      li.style.position = 'relative';
+      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+      i++;
+    })
+
 
 function moveLeft() {
     if (currentPage > 1) {
@@ -22,3 +33,4 @@ function moveRight() {
         list.style.marginLeft = position + 'px'
     }
 }
+
