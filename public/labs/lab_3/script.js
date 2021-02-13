1c33ec34304b6d184a2546cb0f25f38ba4c7c73b
Griffin Barlow <griffinbrlw@gmail.com>
2021-02-12 21:18:53 -0500

    Lab 3 Submission

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,24 @@
-/* Put your javascript in here */
+const list = document.querySelector('ul')
+
+const totalImages = 7
+const visibleImages = 3
+const maxPage = Math.ceil(totalImages/visibleImages)
+
+let position = 0
+let currentPage = 1
+
+function moveLeft() {
+    if (currentPage > 1) {
+        position += 304
+        currentPage--
+        list.style.marginLeft = position + 'px'
+    }
+}
+
+function moveRight() {
+    if (currentPage < maxPage) {
+        position -= 304
+        currentPage++
+        list.style.marginLeft = position + 'px'
+    }
+}
