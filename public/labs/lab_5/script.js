d5484cc6a54579a5438be7d83734a5c33db40fe4
Griffin Barlow <griffinbrlw@gmail.com>
2021-03-09 13:24:35 -0500

    Small changes to make cypress happy

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,6 +1,6 @@
 function mapInit() {
   // follow the Leaflet Getting Started tutorial here
-  let map = L.map('map')
+  let map = L.map('mapid')
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
